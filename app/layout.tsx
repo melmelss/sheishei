abeab287e713f506b5f3de8483306c25b5026638
rodrigolamos24 <rodrigolamos2024@gmail.com>
2025-06-18 21:56:52 -0300

    layt

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Atendimento Whatsapp',
+  description: 'Atendimento automatizado via WhatsApp',
+  generator: 'Whatsapp Dev.',
 }
 
 export default function RootLayout({
